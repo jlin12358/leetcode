6c77d5822bed9c6e7626c0c903731c3e2c880e34
jlin12358 <jlin12358@gmail.com>
2017-07-18 09:59:54 -0700

    two sum problem

---
+++ twoSum.cpp
@@ -0,0 +1,44 @@
+#include <map>
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        /*
+        std::vector<int> v;
+        int i = 0;
+        int j = 0;
+        int k = nums.size();
+        for(i = 0; i < k; i++){
+            for (j = 0; j < k; j++){
+                if ((i != j) && (nums[i] + nums[j] == target)){
+                    std::cout << i << ' ' << j;
+                    std::vector<int>::iterator it = v.end();
+                    it = v.insert(it, j);
+                    it = v.insert(it, i);
+                    return v;
+                }
+            }
+        }
+        return v;
+        */
+        std::vector<int> v;
+        std::map<int, int> m;
+        int check;
+        for (int i = 0; i<nums.size(); i++)
+        {
+            check = target - nums[i];
+            map<int, int>::const_iterator it = m.find(check);
+            if (it != m.end())
+            {
+                v.push_back(m[check]);
+                v.push_back(i);
+                return v;
+            }
+            else
+            {
+                m[nums[i]] = i;
+            }
+        }
+    }
+};
+
