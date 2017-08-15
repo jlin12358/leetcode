58ae4e07c88cb2f9e4ec31a90ab51dc83e1c36c6
jlin12358 <jlin12358@gmail.com>
2017-08-14 17:55:12 -0700

    O(k) solution where k is the number of bits equal to 1

--- hammingDistance.py
+++ hammingDistance.py
@@ -5,9 +5,10 @@ class Solution(object):
         :type y: int
         :rtype: int
         """
-        x = bin(x ^ y)
+        x = x ^ y
         '''
         # O(n) time and O(n) space
+        x = bin(x)
         new = bin(new)
         counter = 0
         for i in range(2,len(new)):
@@ -16,11 +17,19 @@ class Solution(object):
         return counter
     
         '''
-        
+        '''
         # O(n) time and O(1) space
+        x = bin(x)
         counter = 0
         for i in range(2, len(x)):
             if x[i] == '1':
                 counter += 1
         return counter
+        '''
         
+        # O(n) time and O(1) space
+        counter = 0
+        while x:
+            x &= (x-1)
+            counter += 1
+        return counter
